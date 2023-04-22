7897e21bc6539ab0164ab0019682d5f765c2c860
- <-@DESKTOP-OPQ6AQK>
2023-04-22 08:19:05 -0700

    done login

--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import tree from './../images/tree.jpg'; 
+import tree from './../images/tree.jpg';
 
 const Signup = () => {
     return (
@@ -7,11 +7,13 @@ const Signup = () => {
             <div className='login-image'>
                 <img src={tree} />
             </div>
-            <div className='login-input'>
-                <input placeholder="0912..." type="text" />
-                <button>بعدی</button>
-                <h6 >جهت ثبت نام یا ورود شماره موبایل</h6>
-                <h6>خود را وارد کنید</h6>
+            <div className='login-inputs'>
+                <div className='row-login-item'>
+                    <input className='input input-gray signup-input' placeholder="0912..." type="text" />
+                    <button className='btn btn-primary signup-button'>بعدی</button>
+                </div>
+                <h6 className='text'>جهت ثبت نام یا ورود شماره موبایل</h6>
+                <h6 className='text'>خود را وارد کنید</h6>
             </div>
         </div>
     )
