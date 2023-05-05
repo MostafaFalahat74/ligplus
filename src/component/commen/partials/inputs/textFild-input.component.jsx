5aac1cc15ec48fa387b46101a8647773e92b0a58
- <-@DESKTOP-OPQ6AQK>
2023-05-05 09:53:59 -0700

    work on input

---
+++ src/component/commen/partials/inputs/textFild-input.component.jsx
@@ -0,0 +1,6 @@
+import {TextFildInputComponentStyle as S} from './textFild-input.component.style'
+
+
+export const TextFildInputComponent = ({ children}) => {
+    return <S>{children}</S>;
+};
---
+++ src/component/commen/partials/inputs/textFild-input.component.style.jsx
@@ -0,0 +1,13 @@
+import styled,{input} from 'styled-components'
+// const Button = styled.input`
+export const TextFildInputComponentStyle = styled.input`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`;
+// export const TextFildInputComponentStyle={
+//     input
+// }
+// export const TextFildInputComponentStyle={
+//     input
+// }
