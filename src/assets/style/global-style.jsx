2f09aa7b2746414b91e1e160c4b4d9a2fa45d13f
M.Falahat <M.Falahat@FALAHAT-PC>
2023-05-07 12:08:25 +0430

    ass

--- src/assets/style/global-style.jsx
+++ src/assets/style/global-style.jsx
@@ -1,1 +1,33 @@
 import styled from "@emotion/styled/macro";
+const FullCenterDiv = styled.div`
+display: flex;
+align-items: center;
+justify-content: center;
+width: 100vw;
+height: 100vh;
+overflow-x: hidden;
+`;
+const MiddleBorderLine=styled.line`
+border-right: 1px solid gray;
+`;
+const HalfPage=styled.div`
+width: 50%;
+display: flex;
+align-items: center;
+justify-content: center;
+text-align: center;
+`;
+const LoginImg=styled.img`
+width: 80%;
+;`
+
+const Row=styled.div`
+display: block;
+;`
+export const GlobalStyle = {
+    FullCenterDiv,
+    MiddleBorderLine,
+    HalfPage,
+    LoginImg,
+    Row
+}
