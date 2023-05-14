acefda31b33f400bc9d87ebc68fc32efbcea88f4
- <-@DESKTOP-OPQ6AQK>
2023-05-13 20:47:05 -0700

    delete over files

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import Signup from "./component/view/signup";
-// import Authenticate from "./pages/authenticate";
-// import Initialize from "./pages/initialize"
-  import './scss/main.scss'
+import './scss/main.scss'
 import App from './component/App'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
--- src/pages/test.jsx
+++
@@ -1,9 +0,0 @@
-const Test = () => {
-    return (
-        <>
-            gg
-        </>
-    )
-}
-
-export default Test
